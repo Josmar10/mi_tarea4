2dc896a503d13a549c4ba97aebbb1cdb61dedcda
Josmar10 <josmarreyes9875@gmail.com>
2022-07-18 23:44:25 -0400

    Este es mi primer commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import Contacts from './react/Contacts'
+import Form from './react/Form'
+import './css/estilos.css';
+
+
+const URL = "http://www.raydelto.org/agenda.php"
+
+function App() {
+
+  const [postData, setPostData] = useState([])
+  const [data, setData] = useState({
+    nombre: "",
+    apellido: "",
+    telefono: ""
+  })
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => setPostData(data))
+  }, [data])
+
+  const { nombre, apellido, telefono } = data
+
+  function handleChange(e) {
+    let value = e.target.value
+    let name = e.target.name
+    setData({
+      ...data,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    
+    fetch(URL, {
+      method: "POST",
+      body: JSON.stringify({
+        "nombre": nombre,
+        "apellido": apellido,
+        "telefono": telefono
+      })
+    })
+    setData({
+      nombre: "",
+      apellido: "",
+      telefono: ""
+    })
+  }
+
+  return (
+    <>
+      <div className='main_container'>
+        <Form 
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          data={data}
+        />
+        <Contacts 
+          postData={postData}
+        />
+      </div>
+  </>
+  )
+}
+
+export default App;
